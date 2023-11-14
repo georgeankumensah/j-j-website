3695392ee279523448764461c8d3f81aa3079ed6
georgeankumensah <georgeankumensah@gmail.com>
2023-11-14 10:22:04 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/reuseables/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import { NavLink, Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex  justify-between items-center px-[8.75rem] py-[1.38rem]">
+      <Link to="/">
+        <img src={logo} alt="logo" />
+      </Link>
+      <ul
+        className="flex @apply text-[#1E1E1E] gap-[2.56rem] text-lg not-italic font-normal leading-[normal] default-font;
+  "
+      >
+        <NavLink to="/">
+          <li>Home</li>
+        </NavLink>
+        <NavLink to="/">
+          <li>About Us</li>
+        </NavLink>
+        <NavLink to="/">
+          <li>Services</li>
+        </NavLink>
+      </ul>
+      <button className="rounded-[1.875rem] default-font bg-[#3956f0] text-white p-2.5 w-[8.75rem]">
+        Contact Us
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;

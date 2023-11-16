d00d8634010a4f2b3c8d77c5e2eed4671c06bd66
georgeankumensah <georgeankumensah@gmail.com>
2023-11-16 14:15:05 +0000

    fixed route name case bug

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "../components/sections/Hero";
 import About from "../components/sections/About";
 import Services from "../components/sections/Services";
-import Why from "../components/sections/why";
+import Why from "../components/sections/Why";
 import Task from "../components/sections/Task";
 import Connect from "../components/sections/Connect";
 import Space from "../components/sections/Space";

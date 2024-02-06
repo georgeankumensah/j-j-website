0228236475781f2db534360a95bdcf607b1c47b8
GEORGE ANKU-MENSAH <92539173+georgeankumensah@users.noreply.github.com>
2024-02-06 18:22:22 +0000

    fixed footer bug error

--- src/components/reuseables/Footer.jsx
+++ src/components/reuseables/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <section className='w-full bg-[#1c2e88] p-[10px]  text-center text-white text-sm'>
+    <div className='w-full bg-[#1c2e88] p-[10px]  text-center text-white text-sm'>
     <p className=''>&copy; {currentYear} - j & y cleaning services</p>
-  </section>  )
+  </div>  )
 }
 
 export default Footer

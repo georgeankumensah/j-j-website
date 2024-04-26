2f04e223ae6d97336aa1acd252736895c3ac67ea
GEORGE ANKU-MENSAH <92539173+georgeankumensah@users.noreply.github.com>
2024-04-26 21:11:37 +0000

    Update MessageForm.jsx

--- src/components/reuseables/MessageForm.jsx
+++ src/components/reuseables/MessageForm.jsx
@@ -47,7 +47,7 @@ const MessageForm = () => {
           callback(xmlHttp.responseText);
       }
       xmlHttp.open("GET", url, true); // true for asynchronous
-      xmlHttp.send(null);
+      xmlHttp.send(formData);
   }
   
   
